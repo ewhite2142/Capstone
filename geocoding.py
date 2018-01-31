336107ceae7ef0fbe028f75cd3988d3b39182ac9
ewhite2142 <software@ed-white.com>
2018-01-31 11:45:02 -0700

    remove excess docs

--- geocoding.py
+++
@@ -1,9 +0,0 @@
-import googlemaps
-
-gmaps = googlemaps.Client(key='AIzaSyA5hijNL12eH_MHMIKdLicir449saeO6c0')
-
-loc = gmaps.reverse_geocode((46.8529, -113.929))
-
-print("loc:\n{}".format(loc))
-loc
-loc[0]['address_components']
